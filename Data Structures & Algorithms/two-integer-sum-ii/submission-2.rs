687dcbfcfa6a6f2e137bd6b60cb6a1c1c22c873b
Spyder01 <45194214+Spyder01@users.noreply.github.com>
2026-07-04 00:01:59 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut left = 0;
+        let mut right = nums.len().saturating_sub(1);
+
+        while left < right {
+            let sum = nums[left] + nums[right];
+
+            if sum == target {
+                return vec![left as i32 + 1, right as i32 + 1];
+            } 
+
+            if sum < target {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+
+        unreachable!()
+
+    }
+}
